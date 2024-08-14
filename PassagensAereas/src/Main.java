4fbbfbda88b586c6ee92332c570683a9052a31ca
venancio <108436288+grepvenancio@users.noreply.github.com>
2024-08-14 15:58:21 -0300

    add passagens (#10)
    Co-authored-by: laptopfatec <fatec@teste.com>

--- PassagensAereas/src/Main.java
+++ PassagensAereas/src/Main.java
@@ -1,7 +1,12 @@
+import model.Voo;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Main extends JFrame {
     private JPanel MainPanel;
+    private JButton button1;
 
     public Main() {
         setContentPane(MainPanel);
@@ -10,9 +15,23 @@ public class Main extends JFrame {
         setSize(900, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+        JPanel resultPanel = new JPanel();
+        resultPanel.setLayout(new BorderLayout());
     }
 
     public static void main(String[] args) {
-        new Main();
+        Voo novovoo1 = new Voo ("Voo01", "Hell de Janeiro", "Tokyo", 500);
+        Voo novovoo2 = new Voo ("Voo02", "Praia Grande", "Seul", 150);
+        novovoo1.realizarReserva(5);
+        novovoo1.realizarPagamento("ida e volta", true);
+        novovoo1.imprimirpassagem();
+        novovoo2.realizarReserva(10);
+        novovoo2.realizarPagamento("ida", false);
+        novovoo2.imprimirpassagem();
+
+    }
+
+    private void createUIComponents() {
+
     }
 }
